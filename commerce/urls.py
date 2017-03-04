a9bef35c1990abb69dc835aa7fa90275b036c10b
bobbydb129 <bobbydb129@gmail.com>
2017-03-03 20:19:41 -0600

    added home page

--- commerce/urls.py
+++ commerce/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from commerce import views
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = [
 
     url(r'^item/', include('item.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
