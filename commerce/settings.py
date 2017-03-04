5e441738a46048e9178533db2e1ece064be8de65
bobbydb129 <bobbydb129@gmail.com>
2017-03-03 22:02:35 -0600

    added search

--- commerce/settings.py
+++ commerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'item.apps.ItemConfig',
+    'search.apps.SearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- commerce/views.py
+++ commerce/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.contrib.postgres.search import SearchVector
-
+from item.models import *
+from .forms import *
 def index(request):
 	if request.method == 'POST':
-		return HttpResponse("SERCH")
-	context = {}
-	return render(request,"base.html",{})
+		form = SearchForm(request.POST)
+		if form.is_valid():
+			print (form.cleaned_data['search'])
+			items = Item.objects.filter(name__icontains=form.cleaned_data['search'])
+			print (items)
+			return render_to_response('search.html',{'items' : items})
+	else:
+		form = SearchForm();
+	return render(request,"base.html",{'form':form})
 
 #def search(request):
 #	if request.method == 'POST':
