3be5a5d51914c1eb5e0ec4855e6f3068e1f09a40
bobbydb129 <bobbydb129@gmail.com>
2017-03-03 20:43:18 -0600

    added search example

--- commerce/views.py
+++ commerce/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.postgres.search import SearchVector
 
 def index(request):
+	if request.method == 'POST':
+		return HttpResponse("SERCH")
 	context = {}
 	return render(request,"base.html",{})
+
+#def search(request):
+#	if request.method == 'POST':
+#		return HttpResponse("SERCH")
